59129008d4ce378ef892ee100ec9a89db2838ae7
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-05-28 15:51:15 -0500

    debugged error

--- colt-dom/app.js
+++ colt-dom/app.js
@@ -3,3 +3,27 @@ document.getElementById('bear-photo')
 
 //To select the element with id of main:
 document.getElementById('main')
+
+//array like behavior
+const inputs = document.getElementsByTagName("input")
+
+for(let input of inputs){console.log(input.value)} 
+
+//you can actually turn it into an array:
+
+// const arr = [...inputs];
+
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr)
+
+//selecting with classname:
+
+// const special = document.getElementsByClassName("special");
+
+// for(let specials of special){console.log(special)};
+
+const ul = document.getElementsByTagName("ul")[0] //You have 2 Ul's. You forgot to put the [0] hence code was not working initially.
+
+ul.getElementsByClassName("special");

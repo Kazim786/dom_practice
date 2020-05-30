b29fb90e456269f1799fb6c0683b536eae33068f
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-05-29 19:07:46 -0500

    delete button deletes

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li");
+
+var deleter = document.createElement("button");
+deleter.name = "deleter"
+deleter.style.color = "black"
 
 function inputLength() {
 	return input.value.length;
 }
+li[1].appendChild(deleter)
+li[2].appendChild(deleter)
+li[3].appendChild(deleter)
+li[4].appendChild(deleter)
+li[5].appendChild(deleter)
+
 
 function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+	li.appendChild(deleter);
 }
 
 function addListAfterClick() {
@@ -25,6 +37,29 @@ function addListAfterKeypress(event) {
 	}
 }
 
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+deleter.addEventListener("click", function(){
+	
+	li[1].innerText = ""
+	li[2].innerText = ""
+	li[3].innerText = ""
+	li[4].innerText = ""
+	li[5].innerText = ""
+	// li[6].innerText = ""
+})
+
+// function toggle(){
+// 	li.children.classList.toggle("done")
+// }
+
+// li.addEventListener("click", toggle);
+
+//classList.toggle
+
+// 1. If you click on the list item, it toggles the .done  class on and off.
+
+// 2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
